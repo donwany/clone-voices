6d75e1ddcf79b1b4c437142f57b556afdf46c927
donwany <theodondre@gmail.com>
2023-08-11 15:08:46 -0500

    first commit

--- voice.py
+++ voice.py
@@ -3,6 +3,7 @@ text = "Hello, my name is Manmay , how are you?"
 from TTS.tts.configs.bark_config import BarkConfig
 from TTS.tts.models.bark import Bark
 from scipy.io.wavfile import write as write_wavfile
+from IPython.display import Audio
 
 config = BarkConfig()
 model = Bark.init_from_config(config)
@@ -17,4 +18,7 @@ model.to("cuda")
 # It assumes that you have a speaker file in `bark_voices/speaker_n/speaker.wav` or `bark_voices/speaker_n/speaker.npz`
 output_dict = model.synthesize(text, config, speaker_id="speaker", voice_dirs="bark_voices", temperature=0.95)
 
-write_wavfile(filename="/content/output.wav", rate = 24000, data = output_dict['wave'])
+write_wavfile(filename="/content/output.wav", rate = 24000, data = output_dict['wav'])
+
+# play text in notebook
+Audio(output_dict['wav'], rate=24000)
